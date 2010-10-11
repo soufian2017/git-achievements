57257659dd45857a5e28c443d0f2785de82bdff0
staxmanade <jason@elegantcode.com>
2010-10-11 15:54:07 -0700

    Added helper that works on both x86 and x64 machines

--- git-achievements.ps1
+++ git-achievements.ps1
@@ -1,5 +1,28 @@
+function Find-Program($folderName)
+{
+    $p1 = "C:\Program Files\$folderName"
+    if(!(test-path $p1))
+    {
+        $p2 = "C:\Program Files (x86)\$folderName"
+        
+        if(!(test-path $p2))
+        {
+            Write-Warning "Could not find program folder/path in either $p1 or $p2"
+        }
+        else
+        {
+            $p2
+        }
+    }
+    else
+    {
+        $p1
+    }
+}
+
+
 $pathBackup = $Env:Path
 $Env:Path = "/bin;$Env:Path"
 $gaDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-& $Env:ProgramFiles\Git\bin\sh.exe "$gaDir\git-achievements" $args
+&  (Find-Program 'Git\bin\sh.exe') "$gaDir\git-achievements" $args
 $Env:Path = $pathBackup
