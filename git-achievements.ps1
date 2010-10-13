8978398280594d6e598080225e3be7c14b70b476
Jason Jarrett <jason@elegantcode.com>
2010-10-13 15:04:01 -0700

    changed mode of finding the sh.exe path. Should work on both x64 and x86 machines

--- git-achievements.ps1
+++ git-achievements.ps1
@@ -1,28 +1,16 @@
-function Find-Program($folderName)
-{
-    $p1 = "C:\Program Files\$folderName"
-    if(!(test-path $p1))
-    {
-        $p2 = "C:\Program Files (x86)\$folderName"
-        
-        if(!(test-path $p2))
-        {
-            Write-Warning "Could not find program folder/path in either $p1 or $p2"
-        }
-        else
-        {
-            $p2
-        }
-    }
-    else
-    {
-        $p1
-    }
-}
-
 
 $pathBackup = $Env:Path
 $Env:Path = "/bin;$Env:Path"
 $gaDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-&  (Find-Program 'Git\bin\sh.exe') "$gaDir\git-achievements" $args
+
+$gitX = [Environment]::GetEnvironmentVariable("gitAchievementsGitPath", "User");
+if(!(test-path $gitX))
+{
+	$gitEx = 'Git\bin\sh.exe';
+	$gitX = $Env:ProgramFiles, ${Env:ProgramFiles(x86)}, $Env:ProgramW6432 | %{ "$_\$gitEx" } | ?{ Test-Path $_ } | select -first 1
+	[Environment]::SetEnvironmentVariable("gitAchievementsGitPath", $gitX, "User")
+}
+
+&  $gitX "$gaDir\git-achievements" $args
 $Env:Path = $pathBackup
+
